6727562106dec2a0750ad16d056dea1b1056ce52
Артём Соколовский <artem.1990.sokolovskiy@gmail.com>
2019-01-16 22:56:39 +0200

    position block width fix and position text

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 var showMoreButton = document.querySelectorAll('.show-more');
 var showLessButton = document.querySelectorAll('.show-less');
 
@@ -20,27 +20,4 @@ for(let i = 0; i < showLessButton.length; i++) {
         e.target.previousElementSibling.style.display = 'inline';
 
     }, false);
-=======
-var showMoreButton = document.querySelectorAll('.show-more');
-var showLessButton = document.querySelectorAll('.show-less');
-
-for(let i = 0; i < showMoreButton.length; i++) {
-    showMoreButton[i].addEventListener('click', function(e) {
-        console.log(e.target);
-        e.target.parentElement.parentElement.nextElementSibling.style.display = 'block';
-        e.target.style.display = 'none';
-        e.target.nextElementSibling.style.display = 'inline';
-
-    }, false);
-};
-
-for(let i = 0; i < showLessButton.length; i++) {
-    showLessButton[i].addEventListener('click', function(e) {
-        console.log(e.target);
-        e.target.parentElement.parentElement.nextElementSibling.style.display = 'none';
-        e.target.style.display = 'none';
-        e.target.previousElementSibling.style.display = 'inline';
-
-    }, false);
->>>>>>> 27ff8a0f3d2f5a039d2b47d779c8c180dac45b79
 };

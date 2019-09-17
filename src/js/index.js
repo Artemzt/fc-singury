6ab7788918450f7969727791b8b97d4c2033dac1
Артём Соколовский <artem.1990.sokolovskiy@gmail.com>
2019-09-17 20:11:37 +0300

    webpack img fix

--- src/js/index.js
+++ src/js/index.js
@@ -1,43 +1,7 @@
 import '../sass/main.scss'
-
-var showMoreButton = document.querySelectorAll('.show-more');
-var showLessButton = document.querySelectorAll('.show-less');
-
-for(let i = 0; i < showMoreButton.length; i++) {
-    showMoreButton[i].addEventListener('click', function(e) {
-        console.log(e.target);
-        e.target.parentElement.parentElement.nextElementSibling.style.display = 'block';
-        e.target.style.display = 'none';
-        e.target.nextElementSibling.style.display = 'inline';
-
-    }, false);
-};
-
-for(let i = 0; i < showLessButton.length; i++) {
-    showLessButton[i].addEventListener('click', function(e) {
-        console.log(e.target);
-        e.target.parentElement.parentElement.nextElementSibling.style.display = 'none';
-        e.target.style.display = 'none';
-        e.target.previousElementSibling.style.display = 'inline';
+import * as historyView from './views/historyView'
+import * as playersView from './views/playersView'
 
 
-    }, false);
-};
-
-
-
-let historySlider = $('.history-slider');
-let animatedButton = $('.year-link');
-
-for (let i = 0; i < animatedButton.length; i++) {
-    animatedButton[i].addEventListener('click', function (e) {
-        for (let j = 0; j < historySlider.length; j++) {
-            if (historySlider[j].className.includes('history__slider-active')) {
-                historySlider.eq(j).removeClass('history__slider-active');
-                historySlider.eq(j).addClass('history__slider-inactive');
-            }
-        }
-        historySlider.eq(i).removeClass('history__slider-inactive');
-        historySlider.eq(i).addClass('history__slider-active');
-    })
-}
+playersView.showPlayerSection();
+historyView.showHistoryItem();
---
+++ src/js/views/elements.js
@@ -0,0 +1,18 @@
+export const elements = {
+
+    showMoreButton: document.querySelectorAll('.show-more'),
+    showLessButton: document.querySelectorAll('.show-less'),
+    playerSection: {
+        goalkeeper: document.querySelector('.player-section__keeper'),
+        defender: document.querySelector('.player-section__def'),
+        middle: document.querySelector('.player-section__mid'),
+        scorer: document.querySelector('.player-section__scorer'),
+    }
+};
+
+export const elementStrings = {
+    loader: 'loader',
+    historySlider: 'history-slider',
+    animatedButton: 'year-link',
+
+};

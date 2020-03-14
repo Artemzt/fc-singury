0e596eb26890c9aa6db5afd9a583a3ec121acc63
Артём Соколовский <artem.1990.sokolovskiy@gmail.com>
2020-03-14 23:12:07 +0200

    expirements for ios - remove text on history button

--- src/js/views/historyView.js
+++ src/js/views/historyView.js
@@ -53,7 +53,7 @@ export const renderHistoryControls = (history) => {
     const markup = `
     <li class="years-block-conteiner--year year__photo--2017">
         <a href="#${history.year}" class="year-link years-block__button">
-            <div class="history-button__text">${history.year}</div>
+            // <div class="history-button__text">${history.year}</div>
          </a>
     </li>
 `;
